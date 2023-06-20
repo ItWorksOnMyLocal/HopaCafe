32981ce04e8a37b6df8fef7553aa1986c74fb4a4
Barend.Marais <barend.marais@dyna-mo.com>
2023-06-20 11:53:19 +0200

    jhad

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ async function startServer() {
 
     const port = process.env.PORT || 5000;
 
-    app.listen(port,'0.0.0.0', () => {
+    app.listen(port, () => {
       console.log(`Server started on port ${port}`);
     });
   } catch (error) {
